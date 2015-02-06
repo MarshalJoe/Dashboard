5d70e649dbfa8f33993dd14f26c72c830c274f3e
MarshalJoe <joseph.charles.marshall@gmail.com>
2015-02-06 14:50:40 -0600

    Adds vendor scripts

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
 
-  socket.on('update', function(data) {
-    io.emit('update', data);
-  });
+	function updateStats () {
+		var data = new Date().getTime(); 
+		io.emit('update', data);
+	}
 
+
+	setInterval(updateStats, 3000);
 
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+	console.log('listening on port 3000');
 });
 
