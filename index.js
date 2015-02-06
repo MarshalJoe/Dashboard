bd925c6e416a81678dd290cc3e378219b99fbe02
MarshalJoe <joseph.charles.marshall@gmail.com>
2015-02-06 13:37:01 -0600

    Updates same functionality with new variable names

--- index.js
+++ index.js
@@ -6,15 +6,11 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
 
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('update', function(data) {
+    io.emit('update', data);
   });
-
-  // socket.on('update', function(data){
-  //   io.emit('update', data);
-  // });
 
 
 });
